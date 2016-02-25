bb94fb41f4a62945a4cdefd9c5f0c7f2e5a05b19
chaseWilliams <bornofawesomeness@gmail.com>
2016-02-25 18:42:51 -0500

    working ability to fetch image URLs

--- app.rb
+++ app.rb
@@ -2,19 +2,20 @@
 require './flickr/flickr'
 require 'redis'
 require 'sinatra'
-class App < Sinatra::Application
-  include Flickrd
-  def initialize
-    @db = Redis.new
-    @db.set 'cold', [23577541545].to_json
-    @db.set 'fog', [8469962417, 14919486574].to_json
-    @db.set 'snow', [89074472].to_json
-    @db.set 'cool', [12043895515, 20342715613].to_json
-    @db.set 'hot', [23959664094, 9557006394, 16391611278, 8248259072].to_json
-    @db.set 'really_hot', [19656910812, 5951751285].to_json
-    @db.set 'rain', [6845995798, 9615537120, 6133720797, 15274211811].to_json
+flickr = Flickr.new
+#class App < Sinatra::Application
+#  def initialize
+    db = Redis.new
+    db.set 'cold', [23577541545].to_json
+    db.set 'fog', [8469962417, 14919486574].to_json
+    db.set 'snow', [89074472].to_json
+    db.set 'cool', [12043895515, 20342715613].to_json
+    db.set 'warm', [3704273935, 3780893961, 16021074821, 6357276861].to_json
+    db.set 'hot', [23959664094, 9557006394, 16391611278, 8248259072].to_json
+    db.set 'really_hot', [19656910812, 5951751285].to_json
+    db.set 'rain', [6845995798, 9615537120, 6133720797, 15274211811].to_json
     puts 'Redis done!'
-  end
+#  end
   # Remember that this is a helper function that will process each request (regardless of endpoint) before
   # processing the get/post/put sections below
   before do
@@ -23,11 +24,11 @@ class App < Sinatra::Application
   end
 
   # You'll repeat this for each endpoint this app will host and for each HTTP method
-  get '/weather' do
+  get '/picture' do
     # [...your code will go here...]
     temp = params[:temperature]
-    condition_id = params[:id_number]
-    {status: 'ok', data: "#{grab_photo_url determine_photo_id temp, condition_id}"}.to_json
+    condition_id = params[:id_number].to_i
+    {status: 'ok', data: "#{flickr.grab_photo_url(flickr.determine_photo_id(temp, condition_id))}"}.to_json
   end
 
-end
+#end
--- flickr/flickr.rb
+++ flickr/flickr.rb
@@ -1,9 +1,13 @@
 #flickr.rb
 require 'rest-client'
 require 'redis'
-redis = Redis.new
+
 #grabs the image url of a pre-specified photo id using Flickr's API.
-module Flickrd
+class Flickr
+  def initialize
+    @redis = Redis.new
+  end
+
   def grab_photo_url photo_id
     response = JSON.parse (RestClient.get "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=3a86e2e6e0552b135fa3830f8421d07e&format=json&photo_id=#{photo_id}&nojsoncallback=?")
     img_index = nil #default value
@@ -30,26 +34,24 @@ module Flickrd
     hot = get_redis_photo 'hot'
     really_hot = get_redis_photo 'really_hot'
     if (id >= 200 && id < 600) #rain?
-      determine_photo_id rain[rand(rain.length)-1]
+      rain[rand(rain.length)-1]
     elsif id >= 600 && id < 700#snow?
-       determine_photo_id snow[rand(snow.length)-1]
+      snow[rand(snow.length)-1]
     elsif (is_foggy) #fog?
-       determine_photo_id fog[rand(fog.length)-1]
+      fog[rand(fog.length)-1]
     elsif temp <= 10 #cold?
-       determine_photo_id cold[rand(cold.length)-1] #this mechanism returns a random photo id from the array.
+      cold[rand(cold.length)-1] #this mechanism returns a random photo id from the array.
     elsif temp <= 40 #cool?
-       determine_photo_id cool[rand(cool.length)-1]
+      cool[rand(cool.length)-1]
     elsif temp <= 75 #warm?
-       determine_photo_id warm[rand(warm.length)-1]
+      warm[rand(warm.length)-1]
     elsif temp <= 100#hot?
-       determine_photo_id hot[rand(hot.length)-1]
+      hot[rand(hot.length)-1]
     else #probably really hot then.
-       determine_photo_id really_hot[rand(really_hot.length)-1]
+      really_hot[rand(really_hot.length)-1]
     end
   end
-
   def get_redis_photo key
-    JSON.parse redis.get key
+    JSON.parse @redis.get key
   end
-  module_function :grab_photo_url, :determine_photo_id
 end

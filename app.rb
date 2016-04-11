131eb7538f4f18362d6a740aa6be60f07806d8ee
chaseWilliams <bornofawesomeness@gmail.com>
2016-04-10 22:30:43 -0400

    increased reliability with the ENV vars

--- app.rb
+++ app.rb
@@ -4,26 +4,29 @@ require './flickr/loader'
 class App < Sinatra::Application
 
   before do
-      content_type 'application/json'
+    #proxy always returns JSON
+    content_type 'application/json'
   end
 
+  # defines what to do when raising 404
   error 404 do
     Logger.new(STDOUT).error '404 raised'
-    {status: '404', msg: 'Hi, did you mistype your URL? Maybe you meant "endpoint/picture"'}.to_json
+    {status: '404', msg: 'Hi, did you mistype your URL?' +
+     ' For now, just stick with a default "/" endpoint'}.to_json
   end
 
+  #defines what to do when raising 500
   error do
     status 500
     Logger.new(STDOUT).fatal '500 raised'
     {status: 'fail', msg: 'Looks like we done goof\'d'}
   end
 
-  not_found do
-    404
-  end
-
   get '/' do
     flickr = FlickrAPI::Proxy.new({
+        ### Photo IDs ###
+        #These arrays contain the photo IDs of their respective pictures
+        #Stagnant photo IDs should be temporary
                                       cold: [23577541545],
                                       fog: [8469962417, 14919486574],
                                       snow: [89074472],
@@ -32,10 +35,11 @@ class App < Sinatra::Application
                                       hot: [23959664094, 9557006394, 16391611278, 8248259072],
                                       really_hot: [19656910812, 5951751285],
                                       rain: [6845995798, 9615537120, 6133720797, 15274211811]
-                                  }, '3a86e2e6e0552b135fa3830f8421d07e')
+                                  })
     temp = params[:temp].to_i
     condition_id = params[:id].to_i
-    Logger.new(STDOUT).info("Paramters understood to be temp: #{temp} and condition_id: #{condition_id}")
+    Logger.new(STDOUT).info("Paramters understood to be temp: " +
+                     "#{temp} and condition_id: #{condition_id}")
     return {
         status: 'ok',
         url: "#{flickr.grab_photo_url(temp, condition_id)}"
--- flickr/flickr.rb
+++ flickr/flickr.rb
@@ -1,11 +1,12 @@
 module FlickrAPI
   class Proxy
-    def initialize(pics, key)
+    def initialize(pics)
       @pics = pics
-      @key = key
+      @key = ENV['FLICKR_KEY']
       @endpoint = 'api.flickr.com/services/rest'
       @method = 'method=flickr.photos.getSizes'
       @redis = Redis.new(url: ENV['REDIS_URI'])
+
       @logger_out = Logger.new(STDOUT)
       @logger_err = Logger.new(STDERR)
     end
@@ -13,31 +14,31 @@ module FlickrAPI
     def grab_photo_url temp, condition_id
       photo_id = determine_photo_id temp, condition_id
       @logger_out.info 'determine_photo_id called, successfully completed.'
-
-      url = "https://#{@endpoint}/?#{@method}&api_key=#{@key}&format=json&photo_id=#{photo_id}&nojsoncallback=?"
-      @logger_out.info "no existing url, about to send GET request to #{url}"
-      response = JSON.parse RestClient::Request.execute(
-                                url: url,
-                                method: :get,
-                                verify_ssl: false,
-                            )
-      @logger_out.info "Response is #{response.class}, #{response}. First level is #{response['sizes'].class}"
-      index = nil #default value
-      #determines what's the index of the 'Large' image.
-      response['sizes']['size'].each_with_index {|k, i| if k['label'] == 'Large' then index = i; break end}
-      if index.nil? #in case img_index wasn't affected, implying no Large image was found
-         "https://farm7.staticflickr.com/6217/6357276861_1fdc6fe3d4_b.jpg"
-      else
-         picture_url = response['sizes']['size'][index]['source']
+      unless @redis.exists photo_id
+        url = "https://#{@endpoint}/?#{@method}&api_key=#{@key}&format=json&photo_id=#{photo_id}&nojsoncallback=?"
+        @logger_out.info "no existing url, about to send GET request to #{url}"
+        response = JSON.parse RestClient::Request.execute(
+                                  url: url,
+                                  method: :get,
+                                  verify_ssl: false,
+                              )
+        @logger_out.info "Response is #{response.class}, #{response}. First level is #{response['sizes'].class}"
+        index = nil #default value
+        #determines what's the index of the 'Large' image.
+        response['sizes']['size'].each_with_index {|k, i| if k['label'] == 'Large' then index = i; break end}
+        if index.nil? #in case img_index wasn't affected, implying no Large image was found
+           "https://farm7.staticflickr.com/6217/6357276861_1fdc6fe3d4_b.jpg"
+        else
+           picture_url = response['sizes']['size'][index]['source']
+        end
+        @redis.set(photo_id, picture_url)
+        @logger_out.info "Set the url #{picture_url} for photo id #{photo_id} in Redis"
       end
-
-      @redis.set(photo_id, picture_url)
-      @logger_out.info "Set the url #{picture_url} for photo id #{photo_id} in Redis"
 
       photo_url = @redis.get photo_id
       @logger_out.info "Got the url #{photo_url}"
       photo_url
-      end
+    end
 
     private
     def get_photo(key)
